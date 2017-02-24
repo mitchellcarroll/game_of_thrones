70720c73d97215d57b1203422efd9ef7d540158d
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-02-24 12:01:22 -0500

    controllers

--- game_of_thrones/app/controllers/characters_controller.rb
+++ game_of_thrones/app/controllers/characters_controller.rb
@@ -1,3 +1,45 @@
 class CharactersController < ApplicationController
-  # actions will go here
+
+  def index
+    @characters = Character.all
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.create(character_params)
+
+    redirect_to character_path(@character)
+  end
+
+  def show
+    @character = Character.find(params[:name])
+  end
+
+  def edit
+    @character = Character.find(params[:name])
+  end
+
+  def update
+    @character = Character.find(params[:name])
+    @character.update(character_params)
+
+    redirect_to character_path(@character)
+  end
+
+  def destroy
+    @character = Character.find(params[:name])
+    @character.destroy
+
+    redirect_to character_path
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :house)
+  end
 end
+
+# index, edit, create, show, new 
--- game_of_thrones/app/controllers/houses_controller.rb
+++ game_of_thrones/app/controllers/houses_controller.rb
@@ -1,3 +1,37 @@
 class HousesController < ApplicationController
-  # actions will go here
+
+  def index
+    @houses = House.all
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def show
+    @house = House.find(params[:name])
+  end
+
+  def edit
+    @house = House.find(params[:name])
+  end
+
+  def update
+    @house = House.find(params[:name])
+    @house.update(house_params)
+
+    redirect_to house_path(@house)
+  end
+
+  def delete
+    @house = House.find(params[:name])
+    @house.destory
+
+    redirect_to house_path
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name)
+  end
 end
